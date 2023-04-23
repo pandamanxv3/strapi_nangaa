eda3212f163b052ea04e16185ea9ee76006acd07
Adnan <adn.boudjelal@gmail.com>
2023-04-23 08:30:37 +0200

    feat(adherent):adding adherent field to serv

---
+++ src/api/adherent/controllers/adherent.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * adherent controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::adherent.adherent');
---
+++ src/api/adherent/services/adherent.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * adherent service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::adherent.adherent');
