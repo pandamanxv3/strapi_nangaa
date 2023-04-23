4a6bd367a465eb4c5c99e75ecf200f8e25331fa9
Adnan <adn.boudjelal@gmail.com>
2023-04-23 02:10:31 +0200

    feat(a propos, objectifs): ajout de collection

---
+++ src/api/objectif/controllers/objectif.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * objectif controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::objectif.objectif');
---
+++ src/api/objectif/routes/objectif.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * objectif router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::objectif.objectif');
